0eda81807ce7856a2cb1396ccc3199bfe71f5bde
赵铁柱啊赵铁柱 <15525142233@163.com>
2020-11-23 15:48:22 +0800

    梳理页面基本结构

---
+++ src/page/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+   <p>{{msg}}</p>
+ </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:'这是测试内容'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
