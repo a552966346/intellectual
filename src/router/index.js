86e2570d20b85d74de479d5c9d237222f374c3a5
赵铁柱啊赵铁柱 <15525142233@163.com>
2020-11-23 15:49:39 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/page/index/index'
+import index from '@/page/index/index' //主页 index @ 代表src 目录
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'history', //去掉地址栏中的#
   routes: [
+    // 配置路由
     {
       path: '/',
       name: 'index',
