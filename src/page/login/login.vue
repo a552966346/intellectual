3f632e9788e32c0777845b1c76599fd59e844657
赵铁柱啊赵铁柱 <15525142233@163.com>
2020-11-24 15:17:00 +0800

    上传login图片以及文件

---
+++ src/page/login/login.vue
@@ -0,0 +1,32 @@
+<template>
+ <div>
+   <div>
+      <el-image
+      style="width: 100px; height: 100px"
+      :src="url[0]"
+      :fit="fit"></el-image>
+   </div>
+   <div>
+
+   </div>
+   <div>
+
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name:'login',
+  data(){
+    return{
+      url:[
+        '/static/img/logo.png'
+      ]
+    }
+  }
+}
+
+</script>
+<style scoped>
+</style>
