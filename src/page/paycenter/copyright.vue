b983590ad196549063934c79b95d08f6fcbcbdf7
whx9805 <2224121341@qq.com>
2020-11-30 16:29:50 +0800

    版权

--- src/page/paycenter/copyright.vue
+++ src/page/paycenter/copyright.vue
@@ -5,17 +5,111 @@
       <div class="copyright_pay">
         <img src="../../../static/img/paycenter/copyright_top.png" alt="">
         <!-- 分类 -->
-        <div>
-            
+        <div class="copyright_sort">
+            <div class="copyright_select">
+                <dl>
+                    <dt>版权分类</dt>
+                    <dd>
+                       <a href="#"><span>√</span>不限</a>
+                       <a href="#">小说作品</a>
+                       <a href="#">剧本作品</a>
+                       <a href="#">人文社科</a>
+                       <a href="#">美术作品</a>
+                       <a href="#">漫画作品</a>
+                       <a href="#">摄影作品</a>
+                       <a href="#">软件作品</a>
+                       <a href="#">图形作品</a>  
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>交易方式</dt>
+                    <dd>
+                       <a href="#"><span>√</span>不限</a>
+                       <a href="#">转让</a>
+                       <a href="#">授权</a>
+                       <a href="#">分账</a>
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>授权方式</dt>
+                    <dd>
+                       <a href="#"><span>√</span>不限</a>
+                       <a href="#">独家</a>
+                       <a href="#">非独家</a>
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>授权范围</dt>
+                    <dd>
+                       <a href="#"><span>√</span>不限</a>
+                       <a href="#">全部版权</a>
+                       <a href="#">复制权</a>
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>出售价格</dt>
+                    <dd>
+                       <a href="#"><span>√</span>不限</a>
+                       <a href="#">1万以下</a>
+                       <a href="#">1-2万</a>
+                    </dd>
+                </dl>
+            </div>
         </div>
         <!-- 产品列表 --> 
-        <div>
-
+        <div class="copyright_list">
+            <div class="copyright_sleft">
+                <a href="#">
+                    <span>综合排序</span> 
+                </a>
+                <a href="#">
+                    <span>价格排序</span>    
+                </a>
+            </div>
+            <div class="copyright_sright">
+                <a class="copyright_on" href="#">
+                    <img src="../../../static/img/paycenter/copyright_on.png" alt="">
+                </a>
+                <a class="copyright_menu" href="#">
+                    <img src="../../../static/img/paycenter/copyright_menu.png" alt="">
+                </a>
+                <div class="copyright_jian">
+                    <a href="#">
+                        <font> < </font> 
+                    </a>
+                    <a href="#">
+                        <font> > </font>
+                    </a>
+                </div>
+            </div>
         </div>
+        <div class="copyright_product">
+           <div class="copyright_content">
+                <a class="copyright_con" href="#" v-for="(item,index) in arr" :key="index">
+                    <div class="copyright_img">
+                        <img :src="item.url" alt="">           
+                    </div>
+                    <div class="copyright_ti">
+                        <h3>{{item.text}}</h3>
+                        <img src="../../../static/img/paycenter/copyright_like.png" alt="">
+                    </div>
+                    <p>{{item.ban}}</p>
+                    <div class="copyright_price">
+                        <span>￥{{item.price}}</span>
+                        <img src="../../../static/img/paycenter/copyright_right.png" alt="">
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
+
+
+
+
         <!-- 猜你喜欢 -->
         <div class="copyright_like">
             <div class="copyright_title">
-                <img src="../../../static/img/paycenter/copyright_xiao.png" alt="">
                 <div class="copyright_copy">
                     <h2>没有找到您心仪的版权？</h2>
                     <a href="#">
@@ -36,8 +130,6 @@
                 </div>
             </div>
             <div class="copyright_content">
-                
-                
                 <a class="copyright_con" href="#" v-for="(item,index) in arr" :key="index">
                     <div class="copyright_img">
                         <img :src="item.url" alt="">           
@@ -113,12 +205,106 @@ export default {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        text-decoration: none;
+    }
+    /* 版权分类 */
+    .copyright_select{
+        margin: 15px;
+        width: 960px;
+        margin: 20px auto;
+    }
+    .copyright_select dl{
+        border-bottom: 1px dashed #E8E5F1;
+        width: 100%;
+        zoom: 1;
+        position: relative;
+        background: #F9FAFE;
+    }
+    .copyright_select dl dt{
+        width: 110px;
+        display: inline-block;
+    }
+    .copyright_select dl dd{
+        line-height: 24px;
+        padding: 6px 0;
+        background: #fff;
+        margin-left: 110px;
+        display: inline-block;
+    }
+    a{
+        text-decoration: none;
+        color: black;
+    }
+    /* 排序 */
+    .copyright_list{
+        width: 1200px;
+        height: 50px;
+        background: #fff;
+        margin: 20px auto 10px auto;
+        overflow: hidden;
+    }
+    .copyright_list a{
+        display: block;
+        float: left;
+        font-size: 14px;
+        color: #666666;
+        line-height: 50px;
+        border-right: 1px solid #cecece;
+        width: 120px;
+        text-align: center
+    }
+    .copyright_list span{
+        background: url(../../../static/img/paycenter/copyright_desc.png)
+            no-repeat  right center;
+        color: #5c8cd6;
+        padding: 0 10px;
+    }
+    .copyright_sright{
+        float: right;
+        height: 50px;
+        border-left: 1px solid #eee;
+        margin-left: 10px;
+    }
+    .copyright_on>img{
+        width: 13px;
+    }
+    .copyright_jian{
+        float: right;
+        height: 50px;
+        padding: 0 10px;
+    }
+    .copyright_jian a{
+        color: #999;
+        display: block;
+        width: 22px;
+        height: 22px;
+        border: 1px solid #e2e2e2;
+        background: #fff;
+        text-align: center;
+        line-height: 22px;
+        cursor: pointer;
+        margin: 13px 2.5px;
+        float: left;
+    }
+    /* 心仪版权 */
+    .copyright_product{
+        width: 1200px;
+        margin: 0 auto;
+    }
+    .copyright_product>.copyright_content{
+        display: flex;
+        justify-content:space-between ;
+        align-items: center;
+    }
+    .copyright_product  a{
+        background-color: #fff;
+        margin: 10px 0;
     }
     .copyright_pay{
         background: #f6f6f6;
     }
     .copyright_like{
-        width: 75%;
+        width: 1200px;
         background-color: #fff;
         margin: 0 auto;
         padding: 30px 0 15px;
@@ -126,21 +312,19 @@ export default {
     .copyright_title{
         width: 100%;
         position: relative;
-    }
-    .copyright_title>img{
-        position: absolute;
-        top: -4px;
-        left: 25%;
+        background: url(../../../static/img/paycenter/copyright_xiao.png)
+                    no-repeat center center;
+        background-position: 260px 0;
     }
     .copyright_copy{
         margin: 0 auto;
         text-align: center;
-        padding-bottom: 10px;
         padding-right: 14px;
     }
     .copyright_copy h2{
         display: inline-block;
         font-weight: 450;
+        padding-bottom: 10px;
     }
     .copyright_copy p{
         padding: 0px 10px;
@@ -167,7 +351,7 @@ export default {
     }
     .copyright_right{
         width: 25px;
-        border-top: 1px solid #ccc;
+        border-top: 1px solid #edecf4;
         position: absolute;
         bottom: 45%;
         left: 53%;
@@ -195,6 +379,7 @@ export default {
         justify-content: space-around;
         align-items: center;
         flex-wrap: wrap;
+        overflow: hidden;
     }
     .copyright_con{
         width: 18%;
@@ -204,7 +389,7 @@ export default {
         text-decoration: none;
     }
     .copyright_img{
-        height: 230px;
+        height: 210px;
         display: flex;
         align-items: center;
         justify-content: center;
@@ -252,5 +437,6 @@ export default {
         color:#fa7219 ;
         font-weight: bold;
     }
-   
+    
+    
 </style>
