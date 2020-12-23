332828201b692eb0af4a14b3dbcb1a721ba0899c
whx9805 <2224121341@qq.com>
2020-12-23 10:56:37 +0800

    国家申请专利组件

---
+++ src/components/serviceon/advantages.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="advantages_all">
+        <div class="advantages_img">
+            <img src="../../../static/img/service/serverice_apply.png" alt="">
+        </div>
+        <div class="advantages_top">
+
+        </div>
+    </div>  
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
