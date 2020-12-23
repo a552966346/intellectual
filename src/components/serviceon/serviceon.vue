582f34669c084666cd7ab6248cc26948c487ffd5
[随心往] <2868764158@qq.com>
2020-12-23 09:59:44 +0800

    修改 购物车

--- src/components/serviceon/serviceon.vue
+++ src/components/serviceon/serviceon.vue
@@ -32,7 +32,7 @@
 </script>
 
 <style>
-    .serviceon_all{width: 100%;height: 530px;display: flex;justify-content: center;align-items: center;flex-direction: column;padding: 20px;font-size: 14px;}
+    .serviceon_all{width: 100%;display: flex;justify-content: center;align-items: center;flex-direction: column;padding: 20px;font-size: 14px;}
     .serviceon_secrt{width: 100%;height: 204px;display: flex;justify-content: center;align-items: center;flex-direction:column;margin-top: 15px;border-radius: 5px; border: 1px solid #9abded;}
     .serviceon_secon{width: 100%;height: 50px;display: flex; align-items: center;flex-direction: row;padding: 0 15px; border-bottom:1px dashed #9abded;}
     .serviceon_sectr{width: 100%;flex: 1;display: flex;justify-content: center;align-items: center;flex-direction: row;padding: 15px;}
