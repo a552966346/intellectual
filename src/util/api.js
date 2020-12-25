1778d7e381cb4ac893917a6abdad3cdcc0857070
liuyuxia <1843353966@qq.com>
2020-12-25 18:38:15 +0800

    专利

--- src/util/api.js
+++ src/util/api.js
@@ -385,6 +385,24 @@ const api = {
 
         return requestAll.requset(baseUrl + '/api/Trademark/sellcategory');
     },
+    //专利分类数据
+    getrankingdata() {
+
+        return requestAll.requset(baseUrl + '/api/Patents/sellcategory');
+    },
+    getrankingpost(name,category_id,number,fee,mobile,code,type) {
+        let data = {
+                 // 名称,类别,专利号,低价,联系方式,验证码，类型
+                 name:name,
+                 category_id:category_id,
+                 number:number,
+                 fee:fee,
+                 mobile:mobile,
+                 code:code,
+                 type:type,
+        }
+        return requestAll.requset(baseUrl + '/api/Trademark/selldata',data);
+    },
 
     // 测试请求
     getBanner(status) {
