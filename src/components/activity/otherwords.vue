a30eedd5fc6325ad643854ee76d913561487a0c4
wti3717 <yy21700099@163.com>
2021-01-04 17:34:30 +0800

    作品

--- src/components/activity/otherwords.vue
+++ src/components/activity/otherwords.vue
@@ -21,7 +21,30 @@
               </div>
           </div>
        </div>
-       <div class="show">
+                        <ul class="works_list">
+                                <li class="color1" v-for="(item,index) in lists" :key="index+'list'">
+                                        <div class="works_colors" >
+                                                <!-- <router-link class="works_pro" :to="{path:'/service_xlj',query:{ id:item.id}}"> -->
+                                                        <div class="works_sele">
+                                                                <img :src="item.images_text">
+                                                        </div>
+                                                <!-- </router-link> -->
+                                                
+                                        </div>
+
+                                        <p class="works_p1">
+                                                <span>{{item.name}}</span>
+                                        </p>
+                                        <p class="works_p2" v-if="item.categoryid_text">{{item.categoryid_text.name}}</p>
+                                        <p class="works_p3">
+                                                <span class="works_price" v-if="item.fee>=10000"><label>￥</label>{{(item.fee/10000)}}万元</span>
+                                                <span class="works_price" v-else><label>￥</label>{{item.fee}}元</span>
+                                                <img src="../../../static/img/paycenter/copyright_right.png" alt="">
+                                        </p>
+                                        <div class="works_kefu" @click="kefu">立即议价</div>
+                                </li>
+                        </ul>
+       <!-- <div class="show">
             <div class="other_works_item">
                   <div class="other_works_item1">
                     <img src="../../../static/img/activity/other1-1.png" alt="">
@@ -59,7 +82,7 @@
                 <img src="../../../static/img/activity/other3-1.png" alt="">
               </div>
             </div>
-       </div>
+       </div> -->
         
     </div>
 </template>
@@ -69,13 +92,46 @@ export default {
     return {
       list:[
         {tet:'美术作品'},{tet:'摄影作品'},{tet:'雕塑'},{tet:'生活创意作品'},{tet:'影视作品'},{tet:'建筑作品'},{tet:'汉服设计'},{tet:'创意作品'},
+      ],
+      lists:[
+        {images_text:"../../../static/img/activity/other1-1.png",
+        name:'美少女',
+        fee:'59000',},
+        {images_text:"../../../static/img/activity/other2-1.png",
+        name:'名字',
+        fee:'59000',},
+        {images_text:"../../../static/img/activity/other3-1.png",
+        name:'名字',
+        fee:'5900',},
+        {images_text:"../../../static/img/activity/other2-1.png",
+        name:'名字',
+        fee:'59000',},
+        {images_text:"../../../static/img/activity/other1-1.png",
+        name:'美少女',
+        fee:'59000',},
+        {images_text:"../../../static/img/activity/other3-1.png",
+        name:'名字',
+        fee:'9000',},
+        {images_text:"../../../static/img/activity/other2-1.png",
+        name:'名字',
+        fee:'46000',},
+        {images_text:"../../../static/img/activity/other1-1.png",
+        name:'美少女',
+        fee:'59000',},
+        {images_text:"../../../static/img/activity/other3-1.png",
+        name:'名字',
+        fee:'59000',},
       ]
+
 
     };
   },
   methods:{
     shangchuan(){
         this.$emit("shangchuan")
+    },
+    kefu(){
+        this.$emit("kefu")
     }
   }
 }
@@ -187,11 +243,142 @@ export default {
   width: 370px;
  
 }
-
 .other_works_item1{
   flex: 1;
 }
 .other_works_item2{
   flex: 2;
 }
+
+
+.works_list {
+  background-color: #e9e9e9;
+                display: flex;
+                flex-wrap: wrap;
+                padding-top: 10px;
+        }
+
+        .works_list li {
+                list-style: none;
+                width: 19%;
+                padding: 10px;
+                background: #f5f5f5;
+                height: 352px;
+                margin-bottom: 20px;
+                margin-left: 10px;
+        }
+
+        .works_list a {
+                text-decoration: none;
+                color: black;
+                background-color: #f5f5f5;
+        }
+
+        .works_list li>a:first-child {
+                display: block;
+                width: 100%;
+                overflow: hidden;
+        }
+
+        .works_list li:hover .works_coloritem {
+                display: block;
+        }
+
+        .works_list li:hover .works_kefu {
+                display: block;
+                cursor: pointer;
+        }
+
+        .works_list li:hover .works_p2 {
+                display: none;
+        }
+
+        .works_colors {
+                height: 235px;
+        }
+
+        .works_sele>img {
+                width: 100%;
+                padding: 10px;
+                max-height: 210px;
+        }
+
+        .works_sele {
+                height: 90%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+        }
+
+        .works_selector {
+                width: 100%;
+                height: 21px;
+                box-sizing: border-box;
+                padding: 3px;
+                display: flex;
+                justify-content: center;
+        }
+
+        .works_coloritem {
+                width: 20%;
+                height: 15px;
+                box-sizing: border-box;
+                border: 1px solid rgba(255, 255, 255, .8);
+                float: left;
+                cursor: pointer;
+                display: none;
+        }
+
+        .works_p1 {
+                margin: 10px 0;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+        }
+
+        .works_p1>img {
+                width: 15px;
+                margin-right: 10px;
+        }
+
+        .works_p1>span {
+                font-size: 15px;
+                font-weight: 200;
+        }
+
+        .works_p2 {
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                margin: 10px 0;
+                font-size: 12px;
+                color: #ccc;
+        }
+
+        .works_p3 {
+                border-top: 1px solid #cecece;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 10px 9px;
+        }
+
+        .works_p3>span {
+                font-size: 15px;
+                color: #fa7219;
+                font-weight: bold;
+        }
+
+        .works_p3>img {
+                width: 15px;
+        }
+
+        .works_kefu {
+                display: none;
+                background-color: #187fc4;
+                color: #fff;
+                border-radius: 5px;
+                text-align: center;
+                padding: 8px 0;
+        }
 </style>
